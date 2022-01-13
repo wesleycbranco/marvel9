988fa3891b77249827b786eb4b33d25a9f09d6c2
Wesley Castelo Branco da Silva <wbranco@server01.foursys.local>
2022-01-13 13:32:16 -0300

    unsubscribe

--- src/app/main/character-detail/character-detail.component.ts
+++ src/app/main/character-detail/character-detail.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { CharactersService } from 'src/app/core/http/characters.service';
 
 @Component({
@@ -7,18 +8,23 @@ import { CharactersService } from 'src/app/core/http/characters.service';
   templateUrl: './character-detail.component.html',
   styleUrls: ['./character-detail.component.scss']
 })
-export class CharacterDetailComponent implements OnInit {
+export class CharacterDetailComponent implements OnInit, OnDestroy {
 
   list: any
+  subscribe: Subscription = new Subscription();
 
   constructor(
     private route: ActivatedRoute,
     private characterService: CharactersService
-    ) { }
+  ) { }
+
+  ngOnDestroy(): void {
+    this.subscribe.unsubscribe();
+  }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id') || '';
-    this.characterService.getChar(id).subscribe((response: any) => {
+    this.subscribe = this.characterService.getChar(id).subscribe((response: any) => {
       this.list = response.data.results
     })
   }
--- src/app/main/character-list/character-list.component.ts
+++ src/app/main/character-list/character-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { CharactersService } from 'src/app/core/http/characters.service';
 
 @Component({
@@ -7,29 +8,34 @@ import { CharactersService } from 'src/app/core/http/characters.service';
   templateUrl: './character-list.component.html',
   styleUrls: ['./character-list.component.scss']
 })
-export class CharacterListComponent implements OnInit {
+export class CharacterListComponent implements OnInit, OnDestroy {
 
   list: any;
   page: number = 0;
-  orderBy: string = 'name'
+  orderBy: string = 'name';
+  subscribe: Subscription = new Subscription();
 
   constructor(
     private characterService: CharactersService,
     private router: Router
-    ) { }
+  ) { }
+
+
+  ngOnDestroy(): void {
+    this.subscribe.unsubscribe();
+  }
 
   ngOnInit(): void {
     this.getResults(this.orderBy, this.page);
   }
 
   private getResults(orderBy: string = 'name', offset: number = 0): void {
-    this.characterService.getAllCharacters(orderBy, offset).subscribe((response: any) => {
+    this.subscribe = this.characterService.getAllCharacters(orderBy, offset).subscribe((response: any) => {
       this.list = response.data.results
     })
   }
 
   backPage(): void {
-    console.log("chamou back")
     if(this.page !== 0) {
       this.page--
       this.getResults(this.orderBy, this.page);
